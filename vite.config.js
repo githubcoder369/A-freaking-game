8792870e80e54d268b656a11485fb4d28db6d7d8
Github coder <101938608+githubcoder369@users.noreply.github.com>
2023-11-08 20:05:33 +0700

    Added small changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     build: {
-        // Specify your entry point for TypeScript
         rollupOptions: {
           input: {
-            main: './src/main.ts'
+            main: "./index.html"
           }
         }
     }
